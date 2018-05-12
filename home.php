d11efe63ebd37fe545293a99b4018c5d4e36165b
Esy <30pewpew@gmail.com>
2018-05-12 13:58:55 +0800

    Initial commit

---
+++ home.php
@@ -0,0 +1,108 @@
+<?php
+     $connect = mysqli_connect("localhost", "root", "", "testing");
+     session_start();
+     if (isset($_SESSION["username"]))
+     {
+          header("location:php");
+     }
+     if (isset($_POST["login"]))
+     {
+          if (empty($_POST["username"]) && empty($_POST["password"]))
+          {
+               echo '<script>alert("Both fields are required")</script>';
+          }
+          else
+          {
+               $username = mysqli_real_escape_string($connect, $_POST["username"]);
+               $password = mysqli_real_escape_string($connect, $_POST["password"]);
+               $password = md5($password);
+               $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+               $result = mysqli_query($connect, $query);
+               if (mysqli_num_rows($result) > 0)
+               {
+                    $_SESSION['username'] = $username;
+               }
+               else
+               {
+                    echo '<script>alert("Wrong User Details")</script>';
+               }
+          }
+     }
+?>
+
+<!DOCTYPE html>
+<html>
+     <title>CS 121 Lab Exam 2</title>
+     <head>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+     </head>
+
+     <body>
+          <div class="container" style="width: 25%;">
+               <h1 align="center">HOME.PHP</h1>
+
+               <?php 
+               if (isset($_GET["action"]) == "login")
+               {
+               ?>
+
+               <h3>Login</h3>
+               <form method="POST">
+                    <label>Enter Username</label>
+                    <input type="text" name="username" class="form-control" required/>
+                    <input type="text" name="username" class="form-control" required/>
+                    <input type="password" name="password" class="form-control" required/>
+                    <input type="password" name="password_confirm" class="form-control" required/>
+                    <input type="email" name="email" class="form-control" required/>
+                    <input type="First Name" name="firstname" class="form-control" required/>
+                    <input type="Middle Initial" name="middleinitial class" required/>
+                    <input type="lastname" name="lastname" class required/>
+
+               </form>
+               }
+
+
+          </div>
+     </body>
+</html>
+
+
+                <h3 align="center">Login</h3>  
+                <br />  
+                <form method="post">  
+                     <label>Enter Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Enter Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="login" value="Login" class="btn btn-info" />  
+                     <br />  
+                     <p align="center"><a href="index.php">Register</a></p>  
+                </form>  
+                <?php       
+                }  
+                else  
+                {  
+                ?>  
+                <h3 align="center">Register</h3>  
+                <br />  
+                <form method="post">  
+                     <label>Enter Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Enter Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="register" value="Register" class="btn btn-info" />  
+                     <br />  
+                     <p align="center"><a href="index.php?action=login">Login</a></p>  
+                </form>  
+                <?php  
+                }  
+                ?>  
+           </div>  
+      </body>  
+ </html>  
